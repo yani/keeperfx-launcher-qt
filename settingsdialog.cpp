ae5b917a19554dccfddec56444404bae5249880e
Yani <yani@protonmail.com>
2024-09-20 22:24:48 +0200

    add about page to settings

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -1,6 +1,9 @@
 #include "settingsdialog.h"
 #include "ui_settingsdialog.h"
 
+#include "version.h"
+#include "kfxversion.h"
+
 SettingsDialog::SettingsDialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::SettingsDialog)
@@ -9,6 +12,10 @@ SettingsDialog::SettingsDialog(QWidget *parent)
 
     // Make sure this widget starts at the first tab
     ui->tabWidget->setCurrentIndex(0);
+
+    // Set version in about
+    ui->labelKfxLauncherVersion->setText(ui->labelKfxLauncherVersion->text() + LAUNCHER_VERSION);
+    ui->labelKfxVersion->setText(ui->labelKfxVersion->text() + KfxVersion::currentVersion.string);
 }
 
 SettingsDialog::~SettingsDialog()
