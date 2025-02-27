bc8a6f4c282b5200b4d998b1a4b9ee21a5c5127d
Yani <yani@protonmail.com>
2025-02-27 14:32:05 +0100

    set dark theme for windows

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,35 @@
 #include <QSettings>
 #include <QFileInfo>
 
+#include <QPalette>
+#include <QStyleFactory>
+#include <QFontDatabase>
+
+using namespace Qt::StringLiterals;
+
 #include "launchermainwindow.h"
 #include "settings.h"
 #include "version.h"
 #include "launcheroptions.h"
 
+void setDarkTheme() {
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    qApp->setPalette(darkPalette);
+}
 
 void launcherLogFileHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
@@ -87,6 +111,9 @@ int main(int argc, char *argv[])
     Settings::load();
     Settings::copyNewKfxSettingsFromDefault();
     Settings::copyMissingLauncherSettings();
+
+    // Force dark theme
+    setDarkTheme();
 
     // Create the main window and show it
     LauncherMainWindow mainWindow;
