8906526f1d348a77070d40be978fb4511f8d5291
Yani <yani@protonmail.com>
2025-04-01 01:09:14 +0200

    improve translation loading

--- src/translator.cpp
+++ src/translator.cpp
@@ -24,22 +24,50 @@ void Translator::loadPotTranslations(const QString &languageCode)
     QString line;
     QString msgid;
     QString msgstr;
+    bool inMsgid = false;
+    bool inMsgstr = false;
+
+    int translationsLoaded = 0;
 
     while (!in.atEnd()) {
         line = in.readLine().trimmed();
+
         if (line.startsWith("msgid")) {
+            if (!msgid.isEmpty() && !msgstr.isEmpty()) {
+                translations[msgid] = msgstr;
+                translationsLoaded++;
+                msgid.clear();
+                msgstr.clear();
+            }
             msgid = line.mid(7).chopped(1);
+            inMsgid = true;
+            inMsgstr = false;
         } else if (line.startsWith("msgstr")) {
             msgstr = line.mid(8).chopped(1);
-            translations[msgid] = msgstr;
+            inMsgid = false;
+            inMsgstr = true;
+        } else if (inMsgid && line.startsWith("\"")) {
+            msgid += line.mid(1).chopped(1);
+        } else if (inMsgstr && line.startsWith("\"")) {
+            msgstr += line.mid(1).chopped(1);
         }
     }
+
+    if (!msgid.isEmpty() && !msgstr.isEmpty()) {
+        translations[msgid] = msgstr;
+        translationsLoaded++;
+    }
+
+    qDebug() << "Translations loaded:" << translationsLoaded;
 }
 
 QString Translator::translate(const char *context, const char *sourceText, const char *disambiguation, int n) const {
     QString source(sourceText);
     if (translations.contains(source)) {
-        return translations.value(source);
+        QString output(translations.value(source));
+        if(output.isEmpty() == false){
+            return translations.value(source);
+        }
     }
     return source;
 }
