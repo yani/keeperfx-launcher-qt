88e626164b9e9f4db98103aa6dda7516a5d89ba9
Yani <yani@protonmail.com>
2025-04-01 01:09:29 +0200

    add a script to grab all translations

---
+++ grab-translations.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Make sure $(pwd) is populated
+if [[ -n "$(pwd)" ]]; then
+    echo "Current directory: $(pwd)"
+else
+    echo "Failed to retrieve the current directory"
+    exit 1
+fi
+
+# Grab translations
+find $(pwd)/ui/ -name "*.ui" -exec sh -c 'sed -n "/notr=\"true\"/!s/.*<string>\(.*\)<\/string>.*/_\(\"\1\"\);/p" {} > {}.tmp.cpp' \;
+xgettext -c++ -k_ --from-code=UTF-8 -o "$(pwd)/i18n/ui.pot" $(find "$(pwd)/ui/" -name "*.ui.tmp.cpp")
+find $(pwd)/ui/ -name "*.ui.tmp.cpp" -exec rm {} \;
+xgettext --omit-header --qt -c++ -ktr -o "$(pwd)/i18n/translations.pot" $(find "$(pwd)/src/" -name "*.cpp") "$(pwd)/i18n/ui.pot"
+rm $(pwd)/i18n/ui.pot
+
+# Remove information about temporary files
+sed -i 's|\.ui\.tmp\.cpp:[0-9]\+|.ui|g' "$(pwd)/i18n/translations.pot"
+
+# Remove personal path from translation file
+sed -i 's|'"$(pwd)"'||g' "$(pwd)/i18n/translations.pot"
+
+# Count translations
+count=$(grep -c 'msgid "' "$(pwd)/i18n/translations.pot")
+echo "Translation strings: $count"
+
+# Done
+echo "Done"
