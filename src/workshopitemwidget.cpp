a7591353134b103a0c3fabe7cc5ca8b1aa00f433
Yani <yani@protonmail.com>
2025-05-07 17:52:49 +0200

    improve image scaling and caching in main window

--- src/workshopitemwidget.cpp
+++ src/workshopitemwidget.cpp
@@ -45,39 +45,50 @@ void WorkshopItemWidget::setImage(QUrl imageUrl)
     QString cacheDir = QDir::temp().filePath("kfx-launcher-img-cache");
     QDir().mkpath(cacheDir); // Ensure the directory exists
 
-    // Generate a shorter hash (using first 16 chars of SHA-256)
+    // Generate a shorter hash (using first 16 chars of SHA-256) for caching
     QByteArray urlHash = QCryptographicHash::hash(imageUrl.toString().toUtf8(), QCryptographicHash::Sha256).toHex().left(16);
-    QString ext = QFileInfo(imageUrl.toString()).suffix();
-    if (ext.isEmpty()) ext = "png"; // Default to PNG if no extension found
 
+    // Get image file extension
+    QString ext = QFileInfo(imageUrl.toString()).suffix();
+    if (ext.isEmpty()) {
+        ext = "png"; // Default to PNG if no extension found
+    }
+
+    // Get image cache path
     QString cachePath = cacheDir + "/" + urlHash + "." + ext;
 
-    // Get image
-    QImage image;
+    // Get pixmap
+    QPixmap imagePixmap;
     if (QFile::exists(cachePath)) {
-        image.load(cachePath);
+        imagePixmap.load(cachePath);
         qDebug() << "Image loaded from cache:" << cachePath;
     } else {
+        // Download image
+        QImage image;
         image = ApiClient::downloadImage(imageUrl);
-        if (!image.isNull()) {
-            image.save(cachePath);
+
+        // Get if image was downloaded
+        if (image.isNull() == true) {
+            qWarning() << "Failed to download image:" << imageUrl;
+            return;
+        }
+
+        // Create image pixmap
+        imagePixmap = QPixmap::fromImage(image).scaled(ui->frame->width(), ui->frame->height(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+        if (imagePixmap.isNull() == true) {
+            qWarning() << "Failed to convert image to pixmap:" << imageUrl;
+            return;
+        }
+
+        // Cache the image pixmap
+        if (imagePixmap.save(cachePath, ext.toUpper().toLatin1().constData())) {
             qDebug() << "Image saved in cache:" << cachePath;
+        } else {
+            qDebug() << "Failed to cache image pixmap:" << cachePath;
         }
     }
 
-    // Make sure image exists
-    if (image.isNull()) {
-        return;
-    }
-
-    // Set the image
+    // Load image
     QLabel *imageLabel = new QLabel(ui->frame);
-    imageLabel->setPixmap(
-        QPixmap::fromImage(image).scaled(
-            ui->frame->width(),
-            ui->frame->height(),
-            Qt::KeepAspectRatioByExpanding,
-            Qt::FastTransformation
-        )
-    );
+    imageLabel->setPixmap(imagePixmap);
 }
