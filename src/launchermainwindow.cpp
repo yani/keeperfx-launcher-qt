48695c3f10a76682c11df441abd8b67c5045bba8
Yani <yani@protonmail.com>
2025-07-19 17:33:56 +0200

    disable binary certificate verification for the time being

--- src/launchermainwindow.cpp
+++ src/launchermainwindow.cpp
@@ -967,7 +967,7 @@ void LauncherMainWindow::verifyBinaryCertificates()
 
     // List of files to check
     QStringList filesToCheck = {
-        "keeperfx.exe", "keeperfx_hvlog.exe",
+        // "keeperfx.exe", "keeperfx_hvlog.exe",
         // "keeperfx-launcher-qt.exe",
     };
 
